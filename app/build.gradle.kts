32148903206d9eccb29446808c1f21355fc81f63
Muhammad Zohaib Imtiaz <159874349+ZohaibKhanDev@users.noreply.github.com>
2024-08-27 09:55:15 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlinxSerialization)
-    id("kotlin-kapt")
+    id("kotlin-kapt") 
 }
 
 android {
